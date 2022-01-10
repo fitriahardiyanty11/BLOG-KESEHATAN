81787c28d4afa25065f1b97f2c580de485566715
Fitria Hardiyanty Irawan <93498512+fitriahardiyanty11@users.noreply.github.com>
2022-01-10 08:13:34 +0700

    Add files via upload

---
+++ storage/framework/views/9ebbdcd3d7da7dabc08c68aa554cbb5fe5cf0885.php
@@ -0,0 +1,23 @@
+
+<?php $__env->startSection('content'); ?>
+<div class="container">
+      <div class="col-sm">
+        <div class="card" style="width: 80%;">
+            <img src="https://www.emc.id/files/uploads/rticle/image/2021/Jul/12/60ebd34a80865/5-tips-sederhana-menjaga-kesehatan-anak-800x450.jpg?token=80c906f155883dabfd4c0901c71485cb" class="card-img-top" alt="gambar laptop">
+            <div class="card-body">
+              <h5 class="card-title">TIPS MENJAGA KESEHATAN KELUARGA</h5>
+              <p class="card-text"><li>Ajak Keluarga untuk Olahraga Bersama</li>
+Cara pertama untuk menjaga kesehatan keluarga adalah dengan berolahraga bersama. Luangkan waktu Anda dan ajak keluarga Anda di akhir pekan dengan melakukan aktifitas fisik atau olahraga bersama. Anda bisa melakukan jalan santai di seputaran lingkungan rumah, jogging, atau bersepeda. Mengajak keluarga untuk membersihkan rumah jika bisa dibilang olahraga karena semua tubuh akan bergerak. Dengan melakukan olahraga, stamina tubuh Anda dan keluarga akan meningkat dan imun tubuh pun juga akan meningkat sehingga tidak mudah terserang penyakit.
+<li>Memasak Makanan Sendiri demi Kesehatan Keluarga</li>
+Cara kedua agar kesehatan keluarga terjaga adalah dengan membiasakan untuk memakan masakan buatan rumah. Dengan Anda dan keluarga terbiasa untuk selalu makan makanan yang dimasak sendiri maka kesehatan keluarga akan tetap terjaga. Hal ini dikarenakan Anda dan keluarga tahu bahan-bahan yang digunakan untuk memasak dan memastikan gizi dan nutrisinya. Selain menjaga kesehatan keluarga kebiasaan ini juga bisa mempererat ikatan dan keharmonisan keluarga. Masih berkaitan dengan makan, Anda dan keluarga juga harus membiasakan diri untuk sarapan. Dengan sarapan, Anda dan keluarga akan memiliki energi untuk menjalankan aktivitas sehingga tidak mudah lelah dan daya tahan tubuh tetap terjaga.
+<li>Selalu Menjaga Kebersihan</li>
+Cara ketiga dalam menjaga kesehatan keluarga adalah dengan selalu menjaga kebersihan. Ajak dan biasakan keluarga Anda untuk melakukan kebiasaan selalu menjaga kebersihan setiap hari, seperti mencuci tangan sebelum makan atau setelah pergi keluar rumah, menggosok gigi sebelum tidur, mencuci kaki dan ganti baju setelah keluar rumah, rutin mengganti seprai kasur, dan membersihkan rumah. Semua kebiasaan ini akan menjauhkan Anda dan keluarga Anda dari berbagai kuman yang menyebabkan berbagai penyakit.
+<li>Selalu Tanamkan Istirahat itu Penting</li>
+Cara mudah untuk menjaga kesehatan keluarga yang terakhir adalah dengan menanamkan pentingnya istirahat yang cukup. Dengan istirahat yang cukup akan membuat tubuh Anda dan keluarga selalu segar dan fit setiap harinya sehingga di hari esoknya Anda dan keluarga dapat beraktivitas secara maksimal. Jangan biasakan untuk tidur terlalu larut bahkan begadang karena terlalu asik bermain gadget atau menghabiskan waktu untuk melihat-lihat media sosoal.
+</p>
+            </div>
+          </div>
+      </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\blog-kesehatan\resources\views/keluarga.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/d61cb3d768ef3f806c8ca77fa756d6ea272d48aa.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand" href="#">BLOG</span>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/home">KESEHATAN TUBUH</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/mental">KESEHATAN MENTAL</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/keluarga">KELUARGA</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/diet">DIET</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav><?php /**PATH C:\xampp\htdocs\blog-kesehatan\resources\views/layout/navbar.blade.php ENDPATH**/ ?>
